6b2a14980cdba1a309678c33b2d0fe05bd485cc1
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-06-24 16:34:44 +0400

    separated createAuctionCommand from request, adding ownerId from claims principal extension method later

---
+++ src/services/AuctionService/AuctionService.Application/Contracts/Requests/CreateAuctionRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace AuctionService.Application.Contracts.Requests;
+
+public class CreateAuctionRequest
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal DesiredPrice { get; set; }
+    public string Currency { get; set; } = string.Empty;
+
+    public DateTime StartsAt { get; set; }
+    public DateTime EndsAt { get; set; }
+}
--- src/services/AuctionService/AuctionService.Application/Features/Auctions/Commands/Create/CreateAuctionCommand.cs
+++ src/services/AuctionService/AuctionService.Application/Features/Auctions/Commands/Create/CreateAuctionCommand.cs
@@ -1,17 +1,28 @@
 ﻿using AuctionService.Application.Contracts.Models;
+using AuctionService.Application.Contracts.Requests;
 using MediatR;
 
 namespace AuctionService.Application.Features.Auctions.Commands.Create;
 
-public sealed class CreateAuctionCommand : IRequest<AuctionModel>
+public sealed class CreateAuctionCommand : CreateAuctionRequest, IRequest<AuctionModel>
 {
+    public CreateAuctionCommand(
+        Guid ownerId,
+        string title,
+        string description,
+        decimal desiredPrice,
+        string currency,
+        DateTime startsAt,
+        DateTime endsAt)
+    {
+        OwnerId = ownerId;
+        Title = title;
+        Description = description;
+        DesiredPrice = desiredPrice;
+        Currency = currency;
+        StartsAt = startsAt;
+        EndsAt = endsAt;
+    }
+
     public Guid OwnerId { get; set; }
-
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public decimal DesiredPrice { get; set; }
-    public string Currency { get; set; } = string.Empty;
-
-    public DateTime StartsAt { get; set; }
-    public DateTime EndsAt { get; set; }
 }
